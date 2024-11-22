00d6c41e3fedfa996b424a4692012d21713bb513
SanuRaj-K <sanusrk29@gmail.com>
2024-11-22 14:34:52 +0530

    fix

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(
   })
 );
 
-// app.use(cors({ origin: "https://word-count-frontend.vercel.app/",credentials:true }));
+// app.use(cors({ origin: "http://localhost:3000",credentials:true }));
 
 const port = 5001;
 app.use(express.json());
