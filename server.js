966891805bb2372268bc2fa26a6fb4860ad130c1
SanuRaj-K <sanusrk29@gmail.com>
2024-11-22 12:47:17 +0530

    fix

--- server.js
+++ server.js
@@ -6,24 +6,24 @@ const searchRoute = require("./router/seachRoute");
 const app = express();
 dotenv.config();
 
-// app.use(
-//   cors({
-//     origin:
-//       ['https://word-count-frontend.vercel.app','http://localhost:3000'],
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin:
+      ['https://word-count-frontend.vercel.app','http://localhost:3000'],
+    credentials: true,
+  })
+);
 
-app.use(cors({ origin: "https://word-count-frontend.vercel.app",credentials:true }));
+// app.use(cors({ origin: "https://word-count-frontend.vercel.app",credentials:true }));
 
 const port = 5001;
 app.use(express.json());
 app.use("/search", searchRoute);
 
-// mongoose
-//   .connect("mongodb://localhost:27017/wordcount")
-//   .then(() => console.log("connected"))
-//   .catch((err) => console.log(err));
+mongoose
+  .connect("mongodb://localhost:27017/wordcount")
+  .then(() => console.log("connected"))
+  .catch((err) => console.log(err));
 
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING)
